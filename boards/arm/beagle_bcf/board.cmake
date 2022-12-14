565d7f4ff2963b8415abdd5575edd1451c2be24b
Jason Kridner <jkridner@beagleboard.org>
2022-12-14 05:44:47 +0000

    BCF: try to import BSL flash script

--- boards/arm/beagle_bcf/board.cmake
+++ boards/arm/beagle_bcf/board.cmake
@@ -1,8 +1,7 @@
 # Copyright (c) 2020 Erik Larson
+# Copyright (c) 2022 Jason Kridner, BeagleBoard.org Foundation
 #
 # SPDX-License-Identifier: Apache-2.0
 
 board_set_flasher_ifnset(misc-flasher)
-
-# Install cc2538-bsl.py from https://github.com/jadonk/cc2538-bsl
-board_finalize_runner_args(misc-flasher cc2538-bsl.py -e -w -v --bootloader-send-break --append /zephyr/zephyr.bin)
+board_finalize_runner_args(misc-flasher $ENV{ZEPHYR_BASE}/boards/arm/beagle_bcf/cc2538-bsl.py -e -w -v --bootloader-send-break --append /zephyr/zephyr.bin)
