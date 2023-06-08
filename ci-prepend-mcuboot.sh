4f2b6d746933edbb96291f5fc493c90cf8c062f9
Jason Kridner <jkridner@beagleboard.org>
2023-06-07 21:31:14 -0400

    Use 48kB mcuboot

--- ci-prepend-mcuboot.sh
+++ ci-prepend-mcuboot.sh
@@ -2,4 +2,4 @@
 # example: ./ci-prepend-mcuboot.sh freedom mcumgr
 mkdir -p build/$1/$2-w-boot/zephyr
 cp temp/$1/mcuboot/zephyr/zephyr.bin build/$1/$2-w-boot/zephyr/
-dd conv=notrunc bs=1 if=temp/$1/$2/zephyr/zephyr.signed.bin of=build/$1/$2-w-boot/zephyr/zephyr.bin seek=131072
+dd conv=notrunc bs=1 if=temp/$1/$2/zephyr/zephyr.signed.bin of=build/$1/$2-w-boot/zephyr/zephyr.bin seek=49152
