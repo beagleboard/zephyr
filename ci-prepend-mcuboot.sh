f06a40082604fbd8c3e52e2a761918ee0a6f12c4
Jason Kridner <jkridner@beagleboard.org>
2023-06-24 11:02:49 -0400

    simplify CI and add C5 blinky

--- ci-prepend-mcuboot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# example: ./ci-prepend-mcuboot.sh freedom mcumgr
-mkdir -p build/$1/$2-w-boot/zephyr
-cp temp/$1/mcuboot/zephyr/zephyr.bin build/$1/$2-w-boot/zephyr/
-dd conv=notrunc bs=1024 seek=36 if=temp/$1/$2/zephyr/zephyr.signed.bin of=build/$1/$2-w-boot/zephyr/zephyr.bin
