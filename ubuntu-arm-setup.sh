be499d203453f241dc984dc99dbd533c644cf5eb
Jason Kridner <jkridner@beagleboard.org>
2023-06-25 02:35:58 -0400

    WIP: setup scripts

--- ubuntu-arm-setup.sh
+++ ubuntu-arm-setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -ex
-ENVNAME=zephyr-beagle-cc1352-env
+ENVNAME=zephyr-beagle-cc1352
 sudo apt update
 sudo apt install --no-install-recommends -y \
-    cmake \
-    gcc python3-dev \
-    device-tree-compiler \
-    gperf \
-    ccache dfu-util \
-    libsdl2-dev \
-    libxml2-dev libxslt1-dev libssl-dev libturbojpeg0-dev libmagic1 \
-    libtool-bin autoconf automake libusb-1.0-0-dev \
-    python3-tk python3-virtualenv
+  git cmake ninja-build gperf \
+  ccache dfu-util device-tree-compiler wget \
+  python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
+  make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1 \
+  libxml2-dev libxslt1-dev libssl-dev libturbojpeg0-dev \
+  libtool-bin autoconf automake libusb-1.0-0-dev \
+  python3-virtualenv
 wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.15.1/zephyr-sdk-0.15.1_linux-aarch64_minimal.tar.gz
 tar xf zephyr-sdk-0.15.1_linux-aarch64_minimal.tar.gz
 rm zephyr-sdk-0.15.1_linux-aarch64_minimal.tar.gz
