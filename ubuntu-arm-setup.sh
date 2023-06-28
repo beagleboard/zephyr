bf4db33e2cdd376586184d93f7eb64a02f7b046a
Jason Kridner <jkridner@beagleboard.org>
2023-06-28 15:12:09 +0000

    Remove bad libs from Ubuntu setup

--- ubuntu-arm-setup.sh
+++ ubuntu-arm-setup.sh
@@ -5,7 +5,7 @@ sudo apt install --no-install-recommends -y \
   git cmake ninja-build gperf \
   ccache dfu-util device-tree-compiler wget \
   python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
-  make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1 \
+  make gcc libsdl2-dev libmagic1 \
   libxml2-dev libxslt1-dev libssl-dev libturbojpeg0-dev \
   libtool-bin autoconf automake libusb-1.0-0-dev \
   python3-virtualenv
