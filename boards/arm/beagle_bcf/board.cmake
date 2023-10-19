827f260678546856e1d56514ef522d3ad2938e63
Jason Kridner <jkridner@beagleboard.org>
2023-10-19 19:25:26 -0400

    Add cc1352-flasher

--- boards/arm/beagle_bcf/board.cmake
+++ boards/arm/beagle_bcf/board.cmake
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# Download cc2538-bsl.py from https://git.beagleboard.org/beagleconnect/zephyr/cc2538-bsl/-/tags/2.1-bcf
+# Download cc1352-flasher.py from https://git.beagleboard.org/beagleconnect/cc1352-flasher/-/tags/3.0a0
 
 board_set_flasher_ifnset(misc-flasher)
-board_finalize_runner_args(misc-flasher $ENV{ZEPHYR_BASE}/boards/arm/beagle_bcf/cc2538-bsl.py -w)
+board_finalize_runner_args(misc-flasher $ENV{ZEPHYR_BASE}/boards/arm/beagle_bcf/cc1352-flasher.py --bcf)
