6cc547139711254482e3a7425901c35b98b11185
Jason Kridner <jkridner@beagleboard.org>
2024-08-08 09:28:05 -0400

    really move bcf c5 to a new board

--- boards/beagle/beagleconnect_freedom/revision.cmake
+++
@@ -1,9 +0,0 @@
-set(BOARD_REVISIONS "c5" "c7")
-if(NOT DEFINED BOARD_REVISION)
-  set(BOARD_REVISION "c7")
-else()
-  if(NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
-    message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for beagleconnect_freedom. Accepted revisions: ${BOARD_REVISIONS}")
-  endif()
-endif()
-
